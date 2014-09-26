e04e376864fe88cda9175ee5e5eaa86e68f4973a
Stéphane Blondon <stephane.blondon@gmail.com>
2014-09-27 00:36:57 +0200

    the color of first goal is random; minor refactorings

--- src/goals.py
+++ src/goals.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 
 import pygame.image
 import pygame.sprite
@@ -8,8 +9,8 @@ class Goal(pygame.sprite.Sprite):
     def __init__(self):
         super(Goal, self).__init__()
         self._IMAGES = {"blue": "gfx/goal-blue.png", "red": "gfx/goal-red.png"}
-        self._current_color = "blue"
-        self._reset_image()
+        self._current_color = random.choice(self._IMAGES.keys())
+        self.image = self._set_image()
         self.rect = self.image.get_rect()
         self._player_touch_done = 0
         self._player_touch_required = 3
@@ -17,17 +18,14 @@ class Goal(pygame.sprite.Sprite):
         self.level = None
 
     def update(self):
-       if pygame.sprite.collide_rect(self, self.level.red_player) and self._current_color == "red":
+       if (pygame.sprite.collide_rect(self, self.level.red_player) and self._current_color == "red") or \
+          (pygame.sprite.collide_rect(self, self.level.blue_player) and self._current_color == "blue"):
            self._player_touch_done += 1
-           self._current_color = "blue"
-           self._reset_image()
-       elif pygame.sprite.collide_rect(self, self.level.blue_player) and self._current_color == "blue":
-           self._player_touch_done += 1
-           self._current_color = "red"
-           self._reset_image()
+           self._current_color = "blue" if self._current_color == "red" else "red"
+           self.image = self._set_image()
 
-    def _reset_image(self):
-        self.image = pygame.image.load(self._IMAGES[self._current_color]).convert_alpha()
+    def _set_image(self):
+        return pygame.image.load(self._IMAGES[self._current_color]).convert_alpha()
 
     def reached(self):
         return self._player_touch_done >= self._player_touch_required
